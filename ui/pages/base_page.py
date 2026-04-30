43e950b78187f80b78e888e1ee1e75b97be49d78
enockdeghost <enockdeghost@gmail.com>
2026-04-30 11:51:28 +0300

    solveBlankBug

--- ui/pages/base_page.py
+++ ui/pages/base_page.py
@@ -1,40 +1,31 @@
 import flet as ft
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from ui.app import StationeryApp
-
 
 class BasePage:
-    def __init__(self, app: "StationeryApp"):
+    def __init__(self, app):
         self.app = app
-        self.page = app._page          # uses the app's stored page
+        self.page = app._page          
         self.user_id = app.user_id
         self.username = app.username
         self.role = app.role
 
-    def build(self) -> ft.Control:
+    def build(self):
         raise NotImplementedError
 
-    def snack(self, msg: str, color=ft.Colors.GREEN_700):
+    def snack(self, msg, color=ft.Colors.GREEN_700):
         self.app.snack(msg, color)
 
-    def close_dialog(self, dialog: ft.AlertDialog):
+    def close_dialog(self, dialog):
         dialog.open = False
         if dialog in self.page.overlay:
             self.page.overlay.remove(dialog)
         self.page.update()
 
-    def show_dialog(self, dialog: ft.AlertDialog):
+    def show_dialog(self, dialog):
         self.page.overlay.append(dialog)
         dialog.open = True
         self.page.update()
 
-    def dialog_width(self, desktop_w: int = 520) -> int:
+    def dialog_width(self, desktop_w=520):
         if self.page and self.page.width:
             return int(min(desktop_w, self.page.width * 0.94))
         return desktop_w
-
-    def scrollable_table(self, table: ft.DataTable, expand=True, height=None) -> ft.Container:
-        from ui.components.tables import scrollable_table
-        return scrollable_table(table, expand, height)
